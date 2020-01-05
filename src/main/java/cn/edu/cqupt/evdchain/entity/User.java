1163e481dbff6d821a18823beffe47cd4688e166
dylan <dylan.jiacheng.huang@outlook.com>
2020-01-05 20:47:27 +0800

    Add some attributes to `User` class

--- src/main/java/cn/edu/cqupt/evdchain/entity/User.java
+++ src/main/java/cn/edu/cqupt/evdchain/entity/User.java
@@ -11,22 +11,35 @@ public class User {
     @Id
     @GeneratedValue(generator = "paymentableGenerator")
     @GenericGenerator(name = "paymentableGenerator", strategy = "uuid")
+    @Column(name = "id", length = 32)
     private String id;
 
-    @Column(name = "phone")
+    @Column(name = "phone", length = 16)
     private String phone;
 
-    @Column(name = "email")
+    @Column(name = "email", length = 64)
     private String email;
 
-    @Column(name = "idcard")
+    @Column(name = "idcard", length = 32)
     private String idCard;
 
-    @Column(name = "password")
+    @Column(name = "password", length = 32)
     private String password;
 
+    @Column(name = "status", length = 1)
+    private Integer status = 1;
+
+    @Column(name = "first_name", length = 16)
+    private String firstName;
+
+    @Column(name = "last_name", length = 16)
+    private String lastName;
+
+    @Column(name = "gender", length = 1)
+    private Integer gender;
+
     @Column(name = "gmt_created")
-    private Timestamp gmtCreate;
+    private Timestamp gmtCreated;
 
     @Column(name = "gmt_modified")
     private Timestamp gmtModified;
@@ -71,12 +84,44 @@ public class User {
         this.password = password;
     }
 
-    public Timestamp getGmtCreate() {
-        return gmtCreate;
+    public Integer getStatus() {
+        return status;
     }
 
-    public void setGmtCreate(Timestamp gmtCreate) {
-        this.gmtCreate = gmtCreate;
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getGender() {
+        return gender;
+    }
+
+    public void setGender(Integer gender) {
+        this.gender = gender;
+    }
+
+    public Timestamp getGmtCreated() {
+        return gmtCreated;
+    }
+
+    public void setGmtCreated(Timestamp gmtCreated) {
+        this.gmtCreated = gmtCreated;
     }
 
     public Timestamp getGmtModified() {
@@ -95,7 +140,11 @@ public class User {
                 ", email='" + email + '\'' +
                 ", idCard='" + idCard + '\'' +
                 ", password='" + password + '\'' +
-                ", gmtCreate=" + gmtCreate +
+                ", status=" + status +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", gender=" + gender +
+                ", gmtCreated=" + gmtCreated +
                 ", gmtModified=" + gmtModified +
                 '}';
     }
